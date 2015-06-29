1d10dc6af24b3ee03644c188c27ff13fecfd86bb
CrispyGee <Christian@Plotzky.net>
2015-06-30 00:21:21 +0200

    Text fixes

--- SharityOnline/src/main/java/de/hfu/SharityOnline/setup/TimeHelper.java
+++ SharityOnline/src/main/java/de/hfu/SharityOnline/setup/TimeHelper.java
@@ -1,5 +1,6 @@
 package de.hfu.SharityOnline.setup;
 
+
 public class TimeHelper {
   public static final long DAY_IN_MILLIS = 86400000;
   public static final long YEAR_IN_MILLIS = DAY_IN_MILLIS * 365; // Returns 31536000000
